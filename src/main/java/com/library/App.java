385a0b39420c4cda55c974f0006a6d2361c769de
BCN-Asumiko <yakoshko@gmail.com>
2024-07-23 12:01:38 +0200

    add database connection and .env configuration

--- src/main/java/com/library/App.java
+++ src/main/java/com/library/App.java
@@ -1,13 +1,20 @@
 package com.library;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.List;
+
+import com.library.classes.manageBooks;
+
+public class App {
+    public static void main(String[] args) {
+
+        
+        manageBooks bookDAO = new manageBooks();
+
+        // Fetch all books
+        List<String> books = bookDAO.getAllBooks();
+        for (String book : books) {
+            System.out.println("Book Title: " + book);
+        }
     }
+    
 }
