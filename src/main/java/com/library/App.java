cc05074867101500d05c98accb7e897c1655587c
“Flavia <flaviferri90@gmail.com>
2024-07-25 15:02:55 +0200

    sysmoUpdate

--- src/main/java/com/library/App.java
+++ src/main/java/com/library/App.java
@@ -4,10 +4,18 @@ import java.util.List;
 import com.library.classes.books.Books;
 import com.library.classes.books.GetBooks;
 import com.library.classes.books.Table;
+import com.library.classes.books.UpdateBook;
+
 public class App {
     public static void main(String[] args) {
         GetBooks bookDAO = new GetBooks();
         List<Books> books = bookDAO.getAllBooks();
         Table.printTable(books);
-    }
+    
+
+        UpdateBook bookDao = new UpdateBook();
+        System.out.println(bookDao);
+        
+}
+
 }
