a30983f754c25436c39b68e8e96debef3c3f6db5
Laura Pino <lpigueroa@gmail.com>
2024-07-26 09:40:52 +0200

    Function delete completed

--- src/main/java/com/library/App.java
+++ src/main/java/com/library/App.java
@@ -1,16 +1,58 @@
 package com.library;
 
 import java.util.List;
+import java.util.Scanner;
 
 import com.library.classes.books.Books;
+import com.library.classes.books.DeleteBooks;
 import com.library.classes.books.GetBooks;
 
 public class App {
     public static void main(String[] args) {
         GetBooks bookDAO = new GetBooks();
+        DeleteBooks deleteDAO = new DeleteBooks();
+        Scanner scanner = new Scanner(System.in);
 
-        List<Books> books = bookDAO.getAllBooks();
+        while (true) {
+            System.out.println("¿Qué quieres hacer? ");
+            System.out.println("1. Ver listado de libros ");
+            System.out.println("2. Eliminar libro por su ID");
+            System.out.println("3. Eliminar libro por su título");
+            System.out.println("4. Salir");
 
+            int userChoice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (userChoice) {
+
+                case 1:
+                    List<Books> books = bookDAO.getAllBooks();
+                    printBooks(books);
+                    break;
+                case 2: 
+                    System.out.print("Introduce el ID del libro que quieres borrar: ");
+                    int id = scanner.nextInt();
+                    scanner.nextLine();
+                    deleteDAO.deleteBookById(id);
+                    break;
+                case 3: 
+                    System.out.print("Introduce el título del libro que quieres borrar: ");
+                    String title = scanner.nextLine();
+                    deleteDAO.deleteBookByTitle(title);
+                    break;
+                case 4: 
+                    scanner.close();
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Esa no es una opción válida. Inténtalo de nuevo.");
+            }
+        }
+
+        
+    }
+
+    private static void printBooks(List<Books> books) {
         System.out.println(
                 "----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
         System.out.printf("%-10s %-35s %-60s %-20s %-40s %-20s\n", "Book ID", "Title", "Description", "ISBN", "AUTHOR", "GENRE");
@@ -19,7 +61,6 @@ public class App {
 
         for (Books book : books) {
             System.out.println();
-
             printBook(book);
         }
         System.out.println(
